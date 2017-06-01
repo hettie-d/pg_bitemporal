299cef2339ac939e6942fe74ffc3247ffad05d9e
hettie-d <hettie@MacBook-Pro.local>
2017-06-01 09:58:06 -0500

    trying to merge all changes

--- sql/ll_bitemporal_correction.sql
+++ sql/ll_bitemporal_correction.sql
@@ -1,26 +1,25 @@
-CREATE OR REPLACE FUNCTION bitemporal_internal.ll_bitemporal_correction(p_table text
-, p_list_of_fields text -- fields to update
-, p_list_of_values TEXT  -- values to update with
-, p_search_fields TEXT  -- search fields
-, p_search_values TEXT  --  search values
-, p_effective temporal_relationships.timeperiod  -- effective range we are correcting
-)
-RETURNS INTEGER
---does not check whether this is a future assert, can be used to correct future asserted as well
-AS
+CREATE OR REPLACE FUNCTION bitemporal_internal.ll_bitemporal_correction(
+    p_table text,
+    p_list_of_fields text,
+    p_list_of_values text,
+    p_search_fields text,
+    p_search_values text,
+    p_effective temporal_relationships.timeperiod,
+    p_now temporal_relationships.time_endpoint )
+  RETURNS integer AS
 $BODY$
 DECLARE
   v_rowcount INTEGER:=0;
   v_list_of_fields_to_insert text;
   v_table_attr text[];
-  v_now timestamptz              :=now();-- so that we can reference this time as a constant
+  v_now temporal_relationships.time_endpoint:=p_now ;-- for compatiability with the previous version
+ -- v_last_asserted_start temporal_relationships.time_endpoint;
 BEGIN
  v_table_attr := bitemporal_internal.ll_bitemporal_list_of_fields(p_table);
  IF  array_length(v_table_attr,1)=0
       THEN RAISE EXCEPTION 'Empty list of fields for a table: %', p_table; 
   RETURN v_rowcount;
  END IF;
-
  v_list_of_fields_to_insert:= array_to_string(v_table_attr, ',','');
 
  EXECUTE format($u$ UPDATE %s SET asserted = temporal_relationships.timeperiod_range(lower(asserted), %L, '[)')
@@ -60,4 +59,31 @@ BEGIN
  GET DIAGNOSTICS v_rowcount:=ROW_COUNT; 
  RETURN v_rowcount;
 END;
-$BODY$ LANGUAGE plpgsql;
+$BODY$
+  LANGUAGE plpgsql VOLATILE;
+  
+  CREATE OR REPLACE FUNCTION bitemporal_internal.ll_bitemporal_correction(
+    p_table text,
+    p_list_of_fields text,
+    p_list_of_values text,
+    p_search_fields text,
+    p_search_values text,
+    p_effective temporal_relationships.timeperiod)
+  RETURNS integer AS
+  $BODY$
+  declare v_rowcount int;
+  begin
+   select * into v_rowcount from  bitemporal_internal.ll_bitemporal_correction(
+    p_table ,
+    p_list_of_fields ,
+    p_list_of_values ,
+    p_search_fields ,
+    p_search_values,
+    p_effective ,
+    clock_timestamp() );
+    return v_rowcount;
+    END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE;
+  
+  
