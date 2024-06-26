c2e0ce5830facf060573d688e18c80ee51b2e7c0
hettie-d <henr33@gmail.com>
2024-06-26 09:06:01 -0500

    Update ll_bitemporal_insert_select.sql

--- sql/ll_bitemporal_insert_select.sql
+++ sql/ll_bitemporal_insert_select.sql
@@ -22,3 +22,26 @@ BEGIN
      END;    
 $BODY$ LANGUAGE plpgsql;
 
+
+CREATE OR REPLACE FUNCTION bitemporal_internal.ll_bitemporal_insert_select_effecive(p_table text
+,p_list_of_fields text
+,p_select TEXT
+,p_asserted temporal_relationships.timeperiod ) 
+RETURNS INTEGER
+AS
+ $BODY$
+DECLARE
+v_rowcount INTEGER;
+BEGIN
+ EXECUTE format ($i$INSERT INTO %s (%s, effective, asserted )  
+                 select a.*, %L,%L
+                 from (%s) a RETURNING * $i$
+                ,p_table
+                ,p_list_of_fields||',effective'
+                ,p_asserted
+                ,p_select) ;
+     GET DIAGNOSTICS v_rowcount:=ROW_COUNT; 
+     RETURN v_rowcount;         
+     END;    
+$BODY$ LANGUAGE plpgsql;
+
